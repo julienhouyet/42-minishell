6f910bf36790cd1531a9a39b2f15abb93298b17d
julienhouyet <julienhouyet@gmail.com>
2023-12-29 07:27:41 +0100

    Add Comment Demo

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/27 14:41:50 by jhouyet           #+#    #+#             */
-/*   Updated: 2023/12/28 13:15:32 by jhouyet          ###   ########.fr       */
+/*   Updated: 2023/12/29 07:27:25 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int	main(void)
 
 	while (1)
 	{
+		/* test */
 		entry = readline("$ ");
 		if (*entry)
 		{
