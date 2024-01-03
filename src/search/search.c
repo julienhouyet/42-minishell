8de2700e1e32f50d95cdcea601063ef3cddc9935
julienhouyet <julienhouyet@gmail.com>
2024-01-03 13:52:14 +0100

    Add ft_exec_cmd

--- src/search/search.c
+++ src/search/search.c
@@ -6,18 +6,38 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 12:42:40 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/01/03 12:43:08 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/01/03 13:52:02 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+void	ft_exec_cmd(char *command_path, char **argv)
+{
+	pid_t	pid;
+	int		status;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		if (execve(command_path, argv, NULL) == -1)
+		{
+			perror("minishell");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else if (pid < 0)
+		perror("minishell");
+	else
+		waitpid(pid, &status, 0);
+}
+
 void	ft_search_exec_with_path(char **argv)
 {
 	if (access(argv[0], X_OK) == 0)
-		ft_printf("command_path : %s\n", argv[0]);
+		ft_exec_cmd(argv[0], argv);
 	else
-		ft_printf("PATH NOT FOUND\n");
+		ft_printf("%s : Command not found\n", argv[0]);
 }
 
 void	ft_search_exec(char **argv)
@@ -44,7 +64,7 @@ void	ft_search_exec(char **argv)
 		free(command_path);
 	}
 	if (found)
-		ft_printf("command_path : %s\n", command_path);
+		ft_exec_cmd(command_path, argv);
 	else
-		ft_printf("PATH NOT FOUND\n");
+		ft_printf("%s : Command not found\n", argv[0]);
 }
